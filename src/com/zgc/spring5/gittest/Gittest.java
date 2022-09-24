25a8add3cc5a77fc05b7d7109a3748541d68d3ee
zgc <2020317877@qq.com>
2022-09-24 21:29:58 +0800

    cecond commit

--- src/com/zgc/spring5/gittest/Gittest.java
+++ src/com/zgc/spring5/gittest/Gittest.java
@@ -3,5 +3,6 @@ package com.zgc.spring5.gittest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("Git上传成功！");
+        System.out.println("GIt提交，第二版本");
     }
 }
