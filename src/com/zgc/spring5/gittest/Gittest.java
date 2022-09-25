b070eb97badcb4f7e5de3e686a66e1f72c27c5b6
zgc <2020317877@qq.com>
2022-09-25 09:36:04 +0800

    master test

--- src/com/zgc/spring5/gittest/Gittest.java
+++ src/com/zgc/spring5/gittest/Gittest.java
@@ -5,5 +5,6 @@ public class Gittest {
         System.out.println("Git上传成功！");
         System.out.println("GIt提交，第二版本");
         System.out.println("hot-fix被合并！！！");
+        System.out.println("master test");
     }
 }
