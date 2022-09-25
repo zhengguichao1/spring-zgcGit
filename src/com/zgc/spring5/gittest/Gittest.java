2fb2cc0d0a82bc16143a9286fa1a42bb65c0492c
zgc <2020317877@qq.com>
2022-09-25 09:23:22 +0800

    hot-fix merge

--- src/com/zgc/spring5/gittest/Gittest.java
+++ src/com/zgc/spring5/gittest/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("Git上传成功！");
         System.out.println("GIt提交，第二版本");
+        System.out.println("hot-fix被合并！！！");
     }
 }
